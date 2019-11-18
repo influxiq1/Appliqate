0551318300d1bdfc8a4d485a5063a93044ba1a26
influxiq1 <himadribar.influxiq@gmail.com>
2019-11-18 17:49:28 +0530

    update

--- src/app/compaffiliates/compaffiliates.component.ts
+++ src/app/compaffiliates/compaffiliates.component.ts
@@ -20,4 +20,16 @@ export class CompaffiliatesComponent implements OnInit {
   ngOnInit() {
   }
 
+  scrollToTop() {
+    (function smoothscroll() {
+
+        var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+        if (currentScroll > 0) {
+            window.requestAnimationFrame(smoothscroll);
+            window.scrollTo(0, currentScroll - (currentScroll / 8));
+        }
+
+    })();
+}
 }
